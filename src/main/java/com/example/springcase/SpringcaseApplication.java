7ccb7091db8414c43ee2633b69b8ee3192824862
Yiğitcan Abay <abayyigitcan@gmail.com>
2025-08-05 11:08:23 +0000

    Setup Java 21, Spring Boot with Security, JPA, PostgreSQL, Redis, Swagger and Docker Compose

---
+++ src/main/java/com/example/springcase/SpringcaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcase/SpringcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
